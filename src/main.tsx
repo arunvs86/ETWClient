558fd290267367fef2fc4af3dac678317e3b109a
Arun Soundararajan <arunsoundararajan@Aruns-MacBook-Air.local>
2025-09-07 13:39:37 +0100

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
 import './index.css'
@@ -12,11 +12,11 @@ import { AuthProvider } from './context/AuthProvider'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={qc}>
-      <HashRouter>
+      <BrowserRouter>
         <AuthProvider>
           <App />
         </AuthProvider>
-      </HashRouter>
+      </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>
 )
